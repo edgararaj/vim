e9d9ac61d09fa078e630eb545bac9472c06bef75
Edgar Araújo <edgararaj@gmail.com>
2020-12-12 12:14:27 +0000

    Change build.bat again

--- src/build.bat
+++ src/build.bat
@@ -5,4 +5,4 @@ cls
 if not defined DevEnvDir call vcvars64.bat > NUL
 
 cd %~dp0
-nmake -f Make_mvc.mak CPU=AMD64 GUI=yes DEBUG=no FEATURES=big SOUND=no WINVER=0x0600 %1
+nmake -f Make_mvc.mak CPU=AMD64 GUI=yes DEBUG=no FEATURES=normal SOUND=no WINVER=0x0600 %1
